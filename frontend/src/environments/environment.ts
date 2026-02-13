8c19d4f77e1e0a74da4e7cdbd2c6f2653524103d
Ashwin Sudhakar <ashwin2005s@gmail.com>
2026-02-13 22:37:23 +0530

    environment set production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://localhost:3000/api',
-    apiUrl: 'https://kairo-ruby.vercel.app/api',
+    apiUrl: 'https://kairo-olive.vercel.app/api',
     appName: 'Kairo Services',
 };
